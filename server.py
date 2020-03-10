d1274f7fc4ca386474723cc7c1b6d032c8714590
Balint Molnar <balintkilencvenegy@gmail.com>
2020-03-10 09:56:49 +0100

    Add skeleton

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+
+@app.route('/game')
+def game():
+    return render_template('game.html')
+
+
+if __name__ == '__main__':
+    app.run()
