78f25313acfbe20b00fadfa6ad4a2c8f066903bd
ZsoltTirkala <tirkala.zsolt@gmail.com>
2020-03-11 11:47:29 +0100

    Almost working line animation

--- static/js/game.js
+++ static/js/game.js
@@ -1,7 +1,21 @@
-initGame();
-
 function initGame() {
-
-    // Your game can start here, but define separate functions, don't write everything in here :)
-
+    myMove(#line-1);
+    myMove(#line-2);
+    myMove(#line-3);
 }
+
+function myMove(line) {
+    const elem = document.getElementById(line);
+    let position = parseInt(elem.style.top);
+    const id = setInterval(frame, 10);
+    function frame() {
+    if (position === 350) {
+        clearInterval(id);
+    } else {
+        position++;
+        elem.style.top = position + 'px';
+    }
+  }
+}
+
+initGame();
