6551fbd5574d99b2e5dc807a7435f8e24457b134
mike32n <mihaly32horvath@gmail.com>
2020-03-11 14:10:32 +0100

    line animation

--- static/js/game.js
+++ static/js/game.js
@@ -1,24 +1,26 @@
-function initGame() {
-    myMove('line-1');
-    myMove('line-2');
-    myMove('line-3');
-}
+let animId;
+
+
 
 function myMove(line) {
     const elem = document.getElementById(line);
     const style = getComputedStyle(elem);
-    console.log(style.top);
-    let position = parseInt(style.top);
-    const id = setInterval(frame, 10);
-    function frame() {
-    if (position === 1000) {
-        clearInterval(id);
-    } else {
-        position++;
-        elem.style.top = position + 'px';
-    }
-  }
+    let line_current_top = parseInt(style.top);
+    if (line_current_top > 600) {
+            line_current_top = -300;
+        }
+        elem.style.top = String(line_current_top + 3) + 'px';
 }
 
+function repeat() {
+    myMove('line-1');
+    myMove('line-2');
+    myMove('line-3');
+    animId = requestAnimationFrame(repeat);
+}
+
+function initGame() {
+    animId = requestAnimationFrame(repeat);
+}
 
 initGame();
