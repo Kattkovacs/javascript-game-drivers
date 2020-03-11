0b64e8b67761e137097b6b720eee32d8f581bdf0
mike32n <mihaly32horvath@gmail.com>
2020-03-11 14:57:24 +0100

    line animation querySelector

--- static/js/game.js
+++ static/js/game.js
@@ -1,9 +1,14 @@
 let animId;
+let gameOver;
 
-
+// function getStyle(selector) {
+//     const elem = document.querySelector(selector);
+//     const style = getComputedStyle(elem);
+//     return style;
+// }
 
 function myMove(line) {
-    const elem = document.getElementById(line);
+    const elem = document.querySelector(line);
     const style = getComputedStyle(elem);
     let line_current_top = parseInt(style.top);
     if (line_current_top > 600) {
@@ -12,10 +17,40 @@ function myMove(line) {
         elem.style.top = String(line_current_top + 3) + 'px';
 }
 
+function left() {
+    const elem = document.querySelector('#player');
+    const style = getComputedStyle(elem);
+    if (gameOver === false && parseInt(style.left) > 0) {
+        car.css('left', parseInt(car.css('left')) - 5);
+        move_left = requestAnimationFrame(left);
+    }
+}
+
+function right() {
+    if (gameOver === false && parseInt(car.css('left')) < container_width - car_width) {
+        car.css('left', parseInt(car.css('left')) + 5);
+        move_right = requestAnimationFrame(right);
+    }
+}
+
+function up() {
+    if (gameOver === false && parseInt(car.css('top')) > 0) {
+        car.css('top', parseInt(car.css('top')) - 3);
+        move_up = requestAnimationFrame(up);
+    }
+}
+
+function down() {
+    if (gameOver === false && parseInt(car.css('top')) < container_height - car_height) {
+        car.css('top', parseInt(car.css('top')) + 3);
+        move_down = requestAnimationFrame(down);
+    }
+}
+
 function repeat() {
-    myMove('line-1');
-    myMove('line-2');
-    myMove('line-3');
+    myMove('#line-1');
+    myMove('#line-2');
+    myMove('#line-3');
     animId = requestAnimationFrame(repeat);
 }
 
