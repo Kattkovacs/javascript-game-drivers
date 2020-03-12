bf29c373b6a1f216eeea0f3d67e4ad2fba6dc8d8
mike32n <mihaly32horvath@gmail.com>
2020-03-12 10:25:52 +0100

    car placement

--- static/js/game.js
+++ static/js/game.js
@@ -1,6 +1,13 @@
 let animId;
-let gameOver;
+let gameOver = false;
+let scoreCounter = 0;
+let score = document.querySelector('#score');
+let speed = 1;
+let lineSpeed = 5;
 let moveLeft;
+let moveRight;
+let moveUP;
+let moveDown;
 
 // function getStyle(selector) {
 //     const elem = document.querySelector(selector);
@@ -8,51 +15,83 @@ let moveLeft;
 //     return style;
 // }
 
-function myMove(line) {
+function lineMove(line) {
     const elem = document.querySelector(line);
     const style = getComputedStyle(elem);
-    let line_current_top = parseInt(style.top);
-    if (line_current_top > 600) {
-            line_current_top = -300;
+    let lineCurrentTop = parseInt(style.top);
+    if (lineCurrentTop > 600) {
+            lineCurrentTop = -300;
         }
-        elem.style.top = String(line_current_top + 3) + 'px';
+        elem.style.top = String(lineCurrentTop + lineSpeed) + 'px';
 }
+
+function carMove(car) {
+    const elem = document.querySelector(car);
+    const style = getComputedStyle(elem);
+        let carCurrentTop = parseInt(style.top);
+        if (carCurrentTop > 600) { //road.height
+            carCurrentTop = -300;
+            // let carLeft = parseInt(Math.random() * (300 - 70)); //road.width - car.width
+            // elem.style.left = String(carLeft) + 'px';
+        }
+        elem.style.top = String(carCurrentTop + speed) + 'px';
+    }
 
 function left() {
     const elem = document.querySelector('#player');
     const style = getComputedStyle(elem);
     let leftPosition = parseInt(style.left);
     if (gameOver === false && leftPosition > 0) {
-        elem.style.left = String(leftPosition -5 ) + 'px';
+        elem.style.left = String(leftPosition - 5) + 'px';
         moveLeft = requestAnimationFrame(left);
     }
 }
 
 function right() {
-    if (gameOver === false && parseInt(car.css('left')) < container_width - car_width) {
-        car.css('left', parseInt(car.css('left')) + 5);
-        move_right = requestAnimationFrame(right);
+    const elem = document.querySelector('#player');
+    const style = getComputedStyle(elem);
+    let leftPosition = parseInt(style.left);
+    if (gameOver === false && leftPosition < 300 - 70) { //road.width - car.width
+        elem.style.left = String(leftPosition + 5) + 'px';
+        moveRight = requestAnimationFrame(right);
     }
 }
 
 function up() {
-    if (gameOver === false && parseInt(car.css('top')) > 0) {
-        car.css('top', parseInt(car.css('top')) - 3);
-        move_up = requestAnimationFrame(up);
+    const elem = document.querySelector('#player');
+    const style = getComputedStyle(elem);
+    let topPosition = parseInt(style.top);
+    if (gameOver === false && topPosition > 0) {
+        elem.style.top = String(topPosition - 3) + 'px';
+        moveUP = requestAnimationFrame(up);
     }
 }
 
 function down() {
-    if (gameOver === false && parseInt(car.css('top')) < container_height - car_height) {
-        car.css('top', parseInt(car.css('top')) + 3);
-        move_down = requestAnimationFrame(down);
+    const elem = document.querySelector('#player');
+    const style = getComputedStyle(elem);
+    let topPosition = parseInt(style.top);
+    if (gameOver === false && topPosition < 600 - 100) { //container.height - car.height
+        elem.style.top = String(topPosition + 3) + 'px';
+        moveDown = requestAnimationFrame(down);
     }
 }
 
 function repeat() {
-    myMove('#line-1');
-    myMove('#line-2');
-    myMove('#line-3');
+    scoreCounter ++;
+    if (scoreCounter % 500 === 0) {
+            // speed++;
+            lineSpeed++;
+        }
+    // if (scoreCounter % 20 == 0) {
+    //         score.text = parseInt(score.text()) + 1);
+    //     }
+    carMove('#car-1');
+    // carMove('#car-2');
+    // carMove('#car-3');
+    lineMove('#line-1');
+    lineMove('#line-2');
+    lineMove('#line-3');
     animId = requestAnimationFrame(repeat);
 }
 
